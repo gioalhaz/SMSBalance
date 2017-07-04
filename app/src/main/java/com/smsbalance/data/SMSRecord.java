e7f7811c220049ad74029935dc97ef7043133419
gioalhaz <gioalhaz@gmail.com>
2017-07-04 08:55:45 +0400

    Initial

---
+++ app/src/main/java/com/smsbalance/data/SMSRecord.java
@@ -0,0 +1,82 @@
+package com.smsbalance.data;
+
+import java.util.Date;
+
+/**
+ * Created by gioal on 02.07.2017.
+ */
+
+public class SMSRecord {
+    public static int TYPE_INBOX = 1;
+    public static int TYPE_SENT = 2;
+    public static int TYPE_DRAFT = 3;
+
+    private int id;
+    private int threadId;
+    private String address;
+    private int date;
+    private String body;
+    private int deleted;
+    private int type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getThreadId() {
+        return threadId;
+    }
+
+    public void setThreadId(int threadId) {
+        this.threadId = threadId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(int deleted) {
+        this.deleted = deleted;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return address + ": " + body;
+    }
+}
